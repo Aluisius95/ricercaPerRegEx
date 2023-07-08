608cb26962746dbfbb22bb654c4ff18db638e914
Aluisius95 <120209413+Aluisius95@users.noreply.github.com>
2023-07-08 13:00:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ricercaPerRegEx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-usscf1)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = '<h1>Search book</h1>';
+var appTxt = document.getElementById('search');
+const appRes = document.getElementById('result');
+
+var arr = [
+  { autore: 'manzoni', titolo: 'promessi sposi' },
+  { autore: 'virgilio', titolo: 'eneide' },
+  { autore: 'tolkien', titolo: 'the lord of the rings' },
+];
+
+console.log('mi stai disubbidendo');
+function monella() {
+  appRes.innerHTML = '';
+  console.log('mi hai fatto arrabbiare, MONELLA');
+  arr.forEach((element) => {
+    if (element.titolo.search(appTxt.value) != -1)
+      appRes.innerHTML +=
+        '<br>Autore: ' + element.autore + '  Titolo: ' + element.titolo;
+  });
+  if (appTxt.value.length == 0) appRes.innerHTML = '';
+}
+
+appTxt.onkeyup = monella;
